e429646f40df7ce324ca2c8fd06cee2fa4414a28
Ethan Grossbach <egrossba@ucsc.edu>
2021-10-07 19:13:23 -0700

    first

---
+++ src/prefabs/Ball.js
@@ -0,0 +1,21 @@
+class Ball extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+    }
+
+    init(){
+        this.setOrigin(0.5);
+        this.body.useDamping = true;
+        this.body.setCollideWorldBounds();
+        this.setMaxVelocity(MAX_X_VEL, MAX_Y_VEL);
+        this.caught = false;
+    }
+
+
+    update() {
+
+    }
+
+}
